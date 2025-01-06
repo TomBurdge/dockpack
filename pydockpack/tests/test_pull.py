1d8ff4b127134b51c30e8fb779564d8db8d5bdd2
Tom <tomburdge3@gmail.com>
2025-01-06 23:06:45 +0000

    test(python): clean up of path on completion of test with fixture

--- pydockpack/tests/test_pull.py
+++ pydockpack/tests/test_pull.py
@@ -1,10 +1,20 @@
 from pydockpack import pull_docker_image
 import os
+import pytest
+import pathlib
+import shutil
 
 
-def test_pull():
+@pytest.fixture
+def cache_path():
+    file_path = pathlib.Path("./cache/")
+    yield file_path
+    shutil.rmtree(file_path)
+
+
+def test_pull(cache_path):
     image = "maxwellflitton/nan-one"
-    directory = "./cache/two"
+    directory = str(cache_path)
     path = pull_docker_image(image, directory)
     # assert is used in the cargo test, but not needed in python land... - would raise exception
     assert os.path.isdir(path)
