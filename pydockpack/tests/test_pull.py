f3bb0c7038e8775f8396b26f8aa3bd963224acb1
Tom <tomburdge3@gmail.com>
2025-01-06 20:39:52 +0000

    tests(python): add improved assert comment

--- pydockpack/tests/test_pull.py
+++ pydockpack/tests/test_pull.py
@@ -5,5 +5,5 @@ def test_pull():
     image = "maxwellflitton/nan-one"
     directory = "./cache/two"
     path = pull_docker_image(image, directory)
-    # assert is used in the cargo test, but not needed here... - would raise exception
+    # assert is used in the cargo test, but not needed in python land... - would raise exception
     assert os.path.isdir(path)
