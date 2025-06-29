ed5eae4261ab14f5c63277b628493e2aea182a4f
Tom <tomburdge3@gmail.com>
2025-06-29 09:55:18 +0100

    feat: add dockpack_py python c bindings

---
+++ coredockpack/dockpack_py/__init__.py
@@ -0,0 +1,11 @@
+from .dockpack_py import ffi, lib
+
+__all__ = ["ffi", "lib", "unpack_files_from_image"]
+
+
+def unpack_files_from_image(image_name: str, directory: str) -> str:
+    image_name: bytes = image_name.encode("utf-8")
+    directory: bytes = directory.encode("utf-8")
+    return ffi.string(lib.unpack_files_from_image_c(image_name, directory)).decode(
+        "utf-8"
+    )
