9f9156cc64c389179b10270368da15842d8adf9f
Tom <tomburdge3@gmail.com>
2025-06-29 01:42:40 +0100

    refactor: python is in a dir called python like in SurrealML

--- coredockpack/python/__init__.py
+++ coredockpack/python/__init__.py
@@ -6,4 +6,4 @@ __all__ = ["ffi", "lib", "unpack_files_from_image"]
 def unpack_files_from_image(image_name: str, directory: str) -> str:
     image_name: bytes = image_name.encode("utf-8")
     directory: bytes = directory.encode("utf-8")
-    return unpack_files_from_image_c(image_name=image_name, directory=directory)
+    return lib.unpack_files_from_image_c(image_name, directory)
--- coredockpack/python/tests/test_dockpack.py
+++ coredockpack/python/tests/test_dockpack.py
@@ -1,17 +1,16 @@
 #!/usr/bin/env python3
 
-import dockpack_py
+from python import unpack_files_from_image
 import os
 import shutil
 
 
 def test_execute_push():
-    image_name = "maxwellflitton/nan-one".encode("utf-8")
+    image_name = "maxwellflitton/nan-one"
     directory = "./cache/two"
-    directory_arg = directory.encode("utf-8")
     if os.path.exists(directory):
         shutil.rmtree(directory)
 
-    result = dockpack_py.lib.unpack_files_from_image_c(image_name, directory_arg)
+    result = unpack_files_from_image(image_name, directory)
     assert os.path.exists(directory)
     shutil.rmtree(directory)
