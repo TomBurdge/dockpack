6e84e52e999b6b9f2ca15a888950259eb95da553
Tom <tomburdge3@gmail.com>
2025-06-29 01:10:52 +0100

    chore: remove commented rust test

--- coredockpack/dockpack_py/tests/test_dockpack.py
+++ coredockpack/dockpack_py/tests/test_dockpack.py
@@ -15,22 +15,3 @@ def test_execute_push():
     result = dockpack_py.lib.unpack_files_from_image_c(image_name, directory_arg)
     assert os.path.exists(directory)
     shutil.rmtree(directory)
-
-
-#     #[tokio::test]
-#     async fn test_unpack_files_from_image() {
-#         let image = "maxwellflitton/nan-one";
-#         let directory = "./cache/two";
-#         let result = unpack_files_from_image(image, directory).await;
-#
-#         match result {
-#             Ok(ref ok) => println!("Ok: {}", ok),
-#             Err(ref err) => println!("Err: {}", err),
-#         }
-#
-#         assert!(result.is_ok());
-#         let path = result.unwrap();
-#         assert!(Path::new(&path).exists());
-#         fs::remove_dir_all(directory).unwrap();
-#     }
-# }
