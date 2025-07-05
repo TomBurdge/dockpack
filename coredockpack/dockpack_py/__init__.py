a3eb02020c55a8158e8e71b4c5ac3504aa1db3ec
Tom <tomburdge3@gmail.com>
2025-07-05 14:59:09 +0100

    feat: add remaining public api dockpack functionality to python c api

--- coredockpack/dockpack_py/__init__.py
+++ coredockpack/dockpack_py/__init__.py
@@ -4,8 +4,17 @@ __all__ = ["ffi", "lib", "unpack_files_from_image"]
 
 
 def unpack_files_from_image(image_name: str, directory: str) -> str:
-    image_name: bytes = image_name.encode("utf-8")
-    directory: bytes = directory.encode("utf-8")
-    return ffi.string(lib.unpack_files_from_image_c(image_name, directory)).decode(
-        "utf-8"
-    )
+    image: bytes = image_name.encode("utf-8")
+    dir: bytes = directory.encode("utf-8")
+    return ffi.string(lib.unpack_files_from_image_c(image, dir)).decode("utf-8")
+
+
+def build_files_from_image(image_name: str, directory: str) -> str:
+    image: bytes = image_name.encode("utf-8")
+    dir: bytes = directory.encode("utf-8")
+    return ffi.string(lib.build_image_from_files_c(image, dir)).decode("utf-8")
+
+
+def create_dockerfile(directory: str) -> str:
+    dir: bytes = directory.encode("utf-8")
+    return ffi.string(lib.create_dockerfile_c(dir)).decode("utf-8")
